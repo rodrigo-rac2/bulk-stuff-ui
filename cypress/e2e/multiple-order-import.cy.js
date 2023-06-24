5d9fef48ffc9f34ea599e2da1bf84d893dc991c8
Rodrigo Costa <rodrigo.costa@gmail.com>
2023-06-24 01:11:42 -0300

    add: increase tests to 250

--- cypress/e2e/multiple-order-import.cy.js
+++ cypress/e2e/multiple-order-import.cy.js
@@ -10,36 +10,72 @@ describe('Multiple Order Import', () => {
         cy.get('.input-group > .btn').click()
         cy.get('#password').type(Cypress.env('PASS'))
         cy.get('.btn').click()
-        cy.get('[data-kendo-menu-index="2"] > .k-link').click()
+        // cy.get('[data-kendo-menu-index="2"] > .k-link').click()
+        cy.contains('Sales').click()
         cy.contains('Manage Orders').click()
     });
-    it('Uploads 1-20 orders', () => {
-        for (let i = 1; i <= 20; i++) {
+    // it('Uploads 1-20 orders', () => {
+    //     for (let i = 1; i <= 20; i++) {
+    //         uploadOrder(i);
+    //     }
+    // })
+    // it('Uploads 21-40 orders', () => {
+    //     for (let i = 21; i <= 40; i++) {
+    //         uploadOrder(i);
+    //     }
+    // })
+    // it('Uploads 41-60 orders', () => {
+    //     for (let i = 41; i <= 60; i++) {
+    //         uploadOrder(i);
+    //     }
+    // })
+    // it('Uploads 61-80 orders', () => {
+    //     for (let i = 61; i <= 80; i++) {
+    //         uploadOrder(i);
+    //     }
+    // })
+    // it('Uploads 81-100 orders', () => {
+    //     for (let i = 81; i <= 100; i++) {
+    //         uploadOrder(i);
+    //     }
+    // })
+    // it('Uploads 101-120 orders', () => {
+    //     for (let i = 101; i <= 120; i++) {
+    //         uploadOrder(i);
+    //     }
+    // })
+    // it('Uploads 121-140 orders', () => {
+    //     for (let i = 121; i <= 140; i++) {
+    //         uploadOrder(i);
+    //     }
+    // })
+    // it('Uploads 141-160 orders', () => {
+    //     for (let i = 141; i <= 160; i++) {
+    //         uploadOrder(i);
+    //     }
+    // })
+    it('Uploads 161-180 orders', () => {
+        for (let i = 163; i <= 180; i++) {
             uploadOrder(i);
         }
     })
-    it('Uploads 21-40 orders', () => {
-        for (let i = 21; i <= 40; i++) {
+    it('Uploads 181-200 orders', () => {
+        for (let i = 181; i <= 200; i++) {
             uploadOrder(i);
         }
     })
-    it('Uploads 41-60 orders', () => {
-        for (let i = 41; i <= 60; i++) {
+    it('Uploads 201-220 orders', () => {
+        for (let i = 201; i <= 220; i++) {
             uploadOrder(i);
         }
     })
-    it('Uploads 61-80 orders', () => {
-        for (let i = 61; i <= 80; i++) {
+    it('Uploads 221-240 orders', () => {
+        for (let i = 221; i <= 240; i++) {
             uploadOrder(i);
         }
     })
-    it('Uploads 81-100 orders', () => {
-        for (let i = 81; i <= 100; i++) {
-            uploadOrder(i);
-        }
-    })
-    it('Uploads 101-120 orders', () => {
-        for (let i = 101; i <= 120; i++) {
+    it('Uploads 241-260 orders', () => {
+        for (let i = 241; i <= 260; i++) {
             uploadOrder(i);
         }
     })
@@ -51,7 +87,7 @@ function uploadOrder(i) {
     cy.get('button').contains('Create Order').click()
     cy.get('button').contains('Multiple Order').click()
     // enters the order name in the input with placeholder containing string "e.g. December 2020"
-    cy.get('input[placeholder*="e.g. December 2020"]').type(`${dateString} - Multiple Order ${i}`)
+    cy.get('input[placeholder*="e.g. December 2020"]').type(`${dateString} - 5000 Item Multiple Orders ${i}`)
     cy.get('input[type="file"]').selectFile(Cypress.env('ORDERS-FILE'), {force: true})
     cy.get('#confirm-order-update').click()
 }
