625773984458d8d7994e31cd2d56ace76dd2e52b
Rodrigo Costa <60664391+rodrigo-rac2@users.noreply.github.com>
2023-06-24 01:14:27 -0300

    Feature/increase to 250 (#2)
    * split tests in chunks of 20
    
    * add: increase tests to 250
    
    * add: increase tests to 250

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   e2e: {
     defaultCommandTimeout: 60000,
     pageLoadTimeout: 60000,
+    numTestsKeptInMemory: 5,
+    experimentalMemoryManagement: true,
     baseUrl: "https://uatconnectenterprise.kehe.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
