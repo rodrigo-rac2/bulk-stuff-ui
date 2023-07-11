09c25cc1f1a2178ccd8d5834e5a40a2ef868c72c
Rodrigo Costa <rodrigo.costa@gmail.com>
2023-07-11 01:08:02 -0300

    add: 250 orders with 300 line items

--- cypress/e2e/multiple-order-import.cy.js
+++ cypress/e2e/multiple-order-import.cy.js
@@ -14,71 +14,71 @@ describe('Multiple Order Import', () => {
         cy.contains('Sales').click()
         cy.contains('Manage Orders').click()
     });
-    // it('Uploads 1-20 orders', () => {
-    //     for (let i = 1; i <= 20; i++) {
-    //         uploadOrder(i);
-    //     }
-    // })
-    // it('Uploads 21-40 orders', () => {
-    //     for (let i = 21; i <= 40; i++) {
-    //         uploadOrder(i);
-    //     }
-    // })
-    // it('Uploads 41-60 orders', () => {
-    //     for (let i = 41; i <= 60; i++) {
-    //         uploadOrder(i);
-    //     }
-    // })
-    // it('Uploads 61-80 orders', () => {
-    //     for (let i = 61; i <= 80; i++) {
-    //         uploadOrder(i);
-    //     }
-    // })
-    // it('Uploads 81-100 orders', () => {
-    //     for (let i = 81; i <= 100; i++) {
-    //         uploadOrder(i);
-    //     }
-    // })
-    // it('Uploads 101-120 orders', () => {
-    //     for (let i = 101; i <= 120; i++) {
-    //         uploadOrder(i);
-    //     }
-    // })
-    it('Uploads 121-140 orders', () => {
-        for (let i = 121; i <= 140; i++) {
+    it('Uploads 1-20 orders', () => {
+        for (let i = 1; i <= 20; i++) {
             uploadOrder(i);
         }
     })
-    it('Uploads 141-160 orders', () => {
-        for (let i = 141; i <= 160; i++) {
+    it('Uploads 21-40 orders', () => {
+        for (let i = 21; i <= 40; i++) {
             uploadOrder(i);
         }
     })
+    it('Uploads 41-60 orders', () => {
+        for (let i = 41; i <= 60; i++) {
+            uploadOrder(i);
+        }
+    })
+    it('Uploads 61-80 orders', () => {
+        for (let i = 61; i <= 80; i++) {
+            uploadOrder(i);
+        }
+    })
+    it('Uploads 81-100 orders', () => {
+        for (let i = 81; i <= 100; i++) {
+            uploadOrder(i);
+        }
+    })
+    it('Uploads 101-120 orders', () => {
+        for (let i = 101; i <= 120; i++) {
+            uploadOrder(i);
+        }
+    })
+    // it('Uploads 121-140 orders', () => {
+    //     for (let i = 121; i <= 140; i++) {
+    //         uploadOrder(i);
+    //     }
+    // })
+    // it('Uploads 141-160 orders', () => {
+    //     for (let i = 141; i <= 160; i++) {
+    //         uploadOrder(i);
+    //     }
+    // })
     it('Uploads 161-180 orders', () => {
         for (let i = 163; i <= 180; i++) {
             uploadOrder(i);
         }
     })
-    it('Uploads 181-200 orders', () => {
-        for (let i = 181; i <= 200; i++) {
-            uploadOrder(i);
-        }
-    })
-    it('Uploads 201-220 orders', () => {
-        for (let i = 201; i <= 220; i++) {
-            uploadOrder(i);
-        }
-    })
+    // it('Uploads 181-200 orders', () => {
+    //     for (let i = 181; i <= 200; i++) {
+    //         uploadOrder(i);
+    //     }
+    // })
+    // it('Uploads 201-220 orders', () => {
+    //     for (let i = 201; i <= 220; i++) {
+    //         uploadOrder(i);
+    //     }
+    // })
     it('Uploads 221-240 orders', () => {
         for (let i = 221; i <= 240; i++) {
             uploadOrder(i);
         }
     })
-    it('Uploads 241-260 orders', () => {
-        for (let i = 241; i <= 260; i++) {
-            uploadOrder(i);
-        }
-    })
+    // it('Uploads 241-260 orders', () => {
+    //     for (let i = 241; i <= 260; i++) {
+    //         uploadOrder(i);
+    //     }
+    // })
 })
 
 function uploadOrder(i) {
